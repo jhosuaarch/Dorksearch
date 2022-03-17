8128d6953ac89fb9d03ab8d8f66fc082b643e222
KZREVOTICAL <80184758+KZREVOTICAL@users.noreply.github.com>
2022-03-17 19:09:46 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 
-os.system("pip3 install cython")
+os.system("pip install cython")
 os.system("gcc `python-config --cflags --ldflags` -o dork dork.c `python-config --libs`")
 os.remove("dork.c")
 
