bd81386592b991527ee058cbb6ce495f976351ce
KZREVOTICAL <80184758+KZREVOTICAL@users.noreply.github.com>
2022-03-18 14:18:12 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 import os
 
-os.system("pkg install termux-elf-cleaner")
-os.system("pip install cython")
+os.system("pkg install termux-elf-cleaner");os.system("pip install cython")
 os.system("gcc `python-config --cflags --ldflags` -o dork dork.c `python-config --libs`")
-os.remove("dork.c")
-os.system("termux-elf-cleaner dork")
+os.remove("dork.c");os.system("termux-elf-cleaner dork")
 
 print("[!] Done")
